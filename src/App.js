8fb3891ea1b99f8bbdf5dc9d56f5083550275847
Isaque Veras <46972789+isaqueveras@users.noreply.github.com>
2022-09-12 19:47:32 -0300

    estudando children e props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./styles.css";
 
-import IMC from "./components/imc";
+import Grupo from "./components/vingadores/Grupo";
+import Heroi from "./components/vingadores/Heroi";
+// import IMC from "./components/imc";
 
 export default function App() {
-  return <IMC altura="160" peso="68" />;
+  return (
+    // <IMC altura="160" peso="68" />;
+    <Grupo titulo="Vingadores">
+      <Heroi nome="Capitao America" />
+    </Grupo>
+  );
 }
---
+++ src/components/vingadores/Heroi.jsx
@@ -0,0 +1,9 @@
+const Heroi = ({ nome, titulo }) => {
+  return (
+    <h3>
+      Meu nome é {nome} e eu faço parte do grupo {titulo}.
+    </h3>
+  );
+};
+
+export default Heroi;
