e12724e77cb163963696eccea26e3998098248e5
Isaque Veras <isaqueveras@alu.ufc.br>
2022-10-11 21:36:31 -0300

    questao 5

--- prova-01/src/components/Questao04.jsx
+++ prova-01/src/components/Questao04.jsx
@@ -3,26 +3,30 @@ import { useEffect, useState } from "react"
 import api from "../services/api"
 
 const Questao04 = () => {
-  var populacoes = []
+  let populacoes = []
   const [paises, setPaises] = useState()
+  const [continente, setContinente] = useState('africa')
   
   useEffect(() => {
-    api.get(`https://restcountries.com/v2/region/africa?fields=name,population`)
+    api.get(`https://restcountries.com/v2/region/${continente}?fields=name,population`)
     .then((res) => setPaises(res.data))
     .catch((err) => {
       console.error(err)
     })
-  }, [])
-  
+  }, [continente])
   
   return (
-    <div>
+    <div className="mb-4">
       <h1>Questão 4</h1>
-      <b>Maior População: {Math.max(...populacoes)} </b>
+      <b>Maior População: {continente === 'asia' ? Math.min(...populacoes) : Math.max(...populacoes)} </b>
       {paises !== undefined && paises?.map(item => {
-        populacoes.push(item.population)
+        populacoes.push(Number(item.population))
         return <p key={item.name}>{item.name}</p>
       })}
+
+      <button className="btn btn-primary mx-1" onClick={() => setContinente('americas')}>Americas</button>
+      <button className="btn btn-primary mx-1" onClick={() => setContinente('asia')}>Asia</button>
+      <button className="btn btn-primary mx-1" onClick={() => setContinente('africa')}>Africa</button>
     </div>
   )
 }
